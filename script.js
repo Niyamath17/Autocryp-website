b3bc661a2736da66d7e2b1043b379da7907bef40
Niyamath17 <79749532+Niyamath17@users.noreply.github.com>
2022-10-29 20:58:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,111 +1,33 @@
-var button = document.querySelector('.ham-button')
-var slide = document.querySelector('.mobile-nav')
 
- button.addEventListener('click', () =>{
-   slide.classList.toggle('display');
-   button.children[0].classList.toggle('toggle1')
-   button.children[1].classList.toggle('toggle2')
-   button.children[2].classList.toggle('toggle3')
- })
+// == Carousel ==
 
- function changeBg(){
-  var navbar = document.getElementById('nav');
-  var scrollValue = window.scrollY;
-  if(scrollValue < 150){
-      navbar.classList.remove('nav-bgcolor');
-  } else{
-      navbar.classList.add('nav-bgcolor');
-  }
+const carouselSlides = document.querySelectorAll('.carousel__slide');
+const pageLink = document.querySelectorAll('.page__link');
+const carouselList = document.querySelector('#carouselList')
+let slideWidth = carouselSlides[0].offsetWidth + 40;
+
+carouselList.addEventListener('scroll', (e) => {
+    lastKnownScrollPosition = carouselList.scrollLeft;
+    let carouselPos = Math.round(lastKnownScrollPosition / slideWidth);
+    makeActive(carouselPos);
+});
+
+pageLink.forEach(pageBtn => {
+
+    pageBtn.addEventListener('click', (e) => {
+        let slideNumber = pageBtn.getAttribute('data-slide');
+        carouselList.scrollLeft = slideNumber * slideWidth;
+     })
+})
+
+function makeActive(indexNumber){
+    pageLink.forEach( element =>{
+        element.classList.remove('page__link--active')
+    })
+    carouselSlides.forEach(element => {
+        element.classList.remove('slide--active')
+    })
+    carouselSlides[indexNumber].classList.add('slide--active');
+    pageLink[indexNumber].classList.add('page__link--active')
 }
 
-window.addEventListener('scroll', changeBg);
-
-
-function changeText(){
-  document.getElementById("chngeprice1").innerHTML = "$100";
-  document.getElementById("chngeprice2").innerHTML = "$290";
-  document.getElementById("chngeprice3").innerHTML = "$490";
-  document.getElementById("chngetext1").innerHTML = "Per Year";
-  document.getElementById("chngetext2").innerHTML = "Per Year";
-  document.getElementById("chngetext3").innerHTML = "Per Year";
-
-  
-}
-
-function revertBack(){
-  document.getElementById("chngeprice1").innerHTML = "$10";
-  document.getElementById("chngeprice2").innerHTML = "$29";
-  document.getElementById("chngeprice3").innerHTML = "$49";
-  document.getElementById("chngetext1").innerHTML = "Per Month";
-  document.getElementById("chngetext2").innerHTML = "Per Month";
-  document.getElementById("chngetext3").innerHTML = "Per Month";
-}
-
-function dotOne(){
-  document.getElementById("dot1").className = "dotlinks active-card";
-  document.getElementById("dot2").className = "dotlinks";
-  document.getElementById("dot3").className = "dotlinks";
-  document.getElementById("dot4").className = "dotlinks";
-  document.getElementById("dot5").className = "dotlinks";
-  document.getElementById("card1").className = "carousle-cards active-card";
-  document.getElementById("card2").className = "carousle-cards";
-  document.getElementById("card3").className = "carousle-cards";
-  document.getElementById("card4").className = "carousle-cards";
-  document.getElementById("card5").className = "carousle-cards";
-  document.getElementById("cardsall").className = "carousleslide-items";
-}
-
-function dotTwo(){
-  document.getElementById("dot1").className = "dotlinks";
-  document.getElementById("dot2").className = "dotlinks active-card";
-  document.getElementById("dot3").className = "dotlinks";
-  document.getElementById("dot4").className = "dotlinks";
-  document.getElementById("dot5").className = "dotlinks";
-  document.getElementById("card1").className = "carousle-cards";
-  document.getElementById("card2").className = "carousle-cards active-card";
-  document.getElementById("card3").className = "carousle-cards";
-  document.getElementById("card4").className = "carousle-cards";
-  document.getElementById("card5").className = "carousle-cards";
-  document.getElementById("cardsall").className = "carousleslide-items";
-}
-
-function dotThree(){
-  document.getElementById("dot1").className = "dotlinks";
-  document.getElementById("dot2").className = "dotlinks";
-  document.getElementById("dot3").className = "dotlinks active-card";
-  document.getElementById("dot4").className = "dotlinks";
-  document.getElementById("dot5").className = "dotlinks";
-  document.getElementById("card1").className = "carousle-cards";
-  document.getElementById("card2").className = "carousle-cards";
-  document.getElementById("card3").className = "carousle-cards active-card";
-  document.getElementById("card4").className = "carousle-cards";
-  document.getElementById("card5").className = "carousle-cards";
-}
-
-function dotFour(){
-  document.getElementById("dot1").className = "dotlinks";
-  document.getElementById("dot2").className = "dotlinks";
-  document.getElementById("dot3").className = "dotlinks";
-  document.getElementById("dot4").className = "dotlinks active-card";
-  document.getElementById("dot5").className = "dotlinks";
-  document.getElementById("card1").className = "carousle-cards";
-  document.getElementById("card2").className = "carousle-cards";
-  document.getElementById("card3").className = "carousle-cards";
-  document.getElementById("card4").className = "carousle-cards active-card";
-  document.getElementById("card5").className = "carousle-cards";
-  document.getElementById("cardsall").className = "carousleslide-items4";
-}
-
-function dotFive(){
-  document.getElementById("dot1").className = "dotlinks";
-  document.getElementById("dot2").className = "dotlinks";
-  document.getElementById("dot3").className = "dotlinks";
-  document.getElementById("dot4").className = "dotlinks";
-  document.getElementById("dot5").className = "dotlinks active-card";
-  document.getElementById("card1").className = "carousle-cards";
-  document.getElementById("card2").className = "carousle-cards";
-  document.getElementById("card3").className = "carousle-cards";
-  document.getElementById("card4").className = "carousle-cards";
-  document.getElementById("card5").className = "carousle-cards active-card";
-  document.getElementById("cardsall").className = "carousleslide-items4";
-}
